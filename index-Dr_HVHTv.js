28c08db26a4733297a65ede674dcae3ab99c3e86
Aarush Oberoi <connectaarushoberoi@gmail.com>
2026-09-05 17:37:48 +0530

    Add files via upload

--- index-Dr_HVHTv.js
+++ index-Dr_HVHTv.js
@@ -57879,16 +57879,16 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchJson } from "@workspace/api-client-react";
 
 const SOCIAL = [
-  { num: "01", name: "Gmail", handle: "aarushoberoi.professional@gmail.com", href: "mailto:aarushoberoi.professional@gmail.com", icon: (
+  { num: "01", name: "Gmail", handle: "info.opcode@gmail.com", href: "mailto:info.opcode@gmail.com", icon: (
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" /></svg>
   )},
-  { num: "02", name: "YouTube", handle: "@aarushoberoi.professional", href: "https://www.youtube.com/@aarushoberoi.professional", icon: (
+  { num: "02", name: "YouTube", handle: "@OPCODE-IPWS", href: "https://www.youtube.com/@OPCODE-IPWS", icon: (
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" /></svg>
   )},
-  { num: "03", name: "Instagram", handle: "@aarushoberoi.professional", href: "https://www.instagram.com/aarushoberoi.professional/", icon: (
+  { num: "03", name: "Instagram", handle: "@opcode_official", href: "https://www.instagram.com/opcode_official/", icon: (
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zm0 10.162a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z" /></svg>
   )},
-  { num: "04", name: "LinkedIn", handle: "linkedin.com/in/aarushoberoi", href: "https://www.linkedin.com/in/aarushoberoi/", icon: (
+  { num: "04", name: "LinkedIn", handle: "linkedin.com/company/opcodeipws", href: "https://www.linkedin.com/company/opcodeipws/", icon: (
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a-2.065 2.065 0 1 1 0-4.13 2.065 2.065 0 0 1 0 4.13zM7.119 20.452H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" /></svg>
   )},
 ];
@@ -64083,8 +64083,8 @@ function H$() {
 const G$ = [{
     num: "01",
     name: "Gmail",
-    handle: "aarushoberoi.professional@gmail.com",
-    href: "mailto:aarushoberoi.professional@gmail.com",
+    handle: "info.opcode@gmail.com",
+    href: "mailto:info.opcode@gmail.com",
     icon: v.jsx("svg", {
         viewBox: "0 0 24 24",
         xmlns: "http://www.w3.org/2000/svg",
@@ -64095,8 +64095,8 @@ const G$ = [{
 }, {
     num: "02",
     name: "YouTube",
-    handle: "@aarushoberoi.professional",
-    href: "https://www.youtube.com/@aarushoberoi.professional",
+    handle: "@OPCODE-IPWS",
+    href: "https://www.youtube.com/@OPCODE-IPWS",
     icon: v.jsx("svg", {
         viewBox: "0 0 24 24",
         xmlns: "http://www.w3.org/2000/svg",
@@ -64107,8 +64107,8 @@ const G$ = [{
 }, {
     num: "03",
     name: "Instagram",
-    handle: "@aarushoberoi.professional,
-    href: "https://www.instagram.com/aarushoberoi.professional/",
+    handle: "@opcode_official",
+    href: "https://www.instagram.com/opcode_official/",
     icon: v.jsx("svg", {
         viewBox: "0 0 24 24",
         xmlns: "http://www.w3.org/2000/svg",
@@ -64119,8 +64119,8 @@ const G$ = [{
 }, {
     num: "04",
     name: "LinkedIn",
-    handle: "linkedin.com/in/aarushoberoi",
-    href: "https://www.linkedin.com/in/aarushoberoi/",
+    handle: "linkedin.com/company/opcodeipws",
+    href: "https://www.linkedin.com/company/opcodeipws/",
     icon: v.jsx("svg", {
         viewBox: "0 0 24 24",
         xmlns: "http://www.w3.org/2000/svg",
@@ -64242,9 +64242,9 @@ function W$() {
                 }), v.jsx("p", {
                     children: "For applications, collaborations, or event related queries, reach us at"
                 }), v.jsx("a", {
-                    href: "mailto:aarushoberoi.professional@gmail.com",
+                    href: "mailto:info.opcode@gmail.com",
                     className: "join__email",
-                    children: "aarushoberoi.professional@gmail.com"
+                    children: "info.opcode@gmail.com"
                 })]
             })
         })]
